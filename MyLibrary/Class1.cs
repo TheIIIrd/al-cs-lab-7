56f27113971ea3d553a90474bc2b47cbe39f8244
TheIIIrd <93443446+TheIIIrd@users.noreply.github.com>
2024-10-19 11:00:00 +0300

    Добавил задание 1

---
+++ MyLibrary/Class1.cs
@@ -0,0 +1,21 @@
+﻿[Comment("Класс представляющий пользователя.")]
+public class User
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+}
+
+[Comment("Класс представляющий продукт.")]
+public class Product
+{
+    public string ProductName { get; set; }
+    public decimal Price { get; set; }
+}
+
+[Comment("Класс представляющий заказ.")]
+public class Order
+{
+    public User Customer { get; set; }
+    public Product Product { get; set; }
+    public int Quantity { get; set; }
+}
